c1461157bf50f1981c00e3c9a0ca00757f55784a
arjunagbt <arjunagbt@gmail.com>
2018-08-27 18:42:19 +0700

    belum ada kabisat & 30 31

---
+++ exercise-4.js
@@ -0,0 +1,108 @@
+var tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 4; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 1953; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var bulanStr = ''
+
+switch (bulan) {
+  case 1:
+    bulanStr = 'Januari'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 2:
+    bulanStr = 'Februari'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 3:
+    bulanStr = 'Maret'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 4:
+    bulanStr = 'April'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 5:
+    bulanStr = 'Mei'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 6:
+    bulanStr = 'Juni'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 7:
+    bulanStr = 'Juli'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 8:
+    bulanStr = 'Agustus'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 9:
+    bulanStr = 'September'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 10:
+    bulanStr = 'Oktober'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 11:
+    bulanStr = 'November'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  case 12:
+    bulanStr = 'Desember'
+    if ((tanggal > 0 && tanggal <=31) && (tahun >= 1900 && tahun <= 2200)){
+      console.log(`${tanggal} ${bulanStr} ${tahun}`)
+    } else {
+      console.log('Masukkan nilai yang valid!')
+    }
+    break;
+  default:
+    console.log('Masukkan nilai yang valid!')
+    break;
+}
+
+
